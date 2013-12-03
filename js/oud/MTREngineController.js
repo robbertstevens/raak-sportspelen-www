269ebfd83417d5edce39dc070615abd6b72d5f5e
Robbert Stevens <robbertstevens@gmail.com>
2013-12-03 13:57:07 +0100

    JS files achirve

---
+++ js/oud/MTREngineController.js
@@ -0,0 +1,14 @@
+/// <reference path="references.ts" />
+var MTREngineController = (function () {
+    function MTREngineController() {
+    }
+    MTREngineController.prototype.draw = function () {
+    };
+    MTREngineController.prototype.onTouchStart = function (e) {
+    };
+    MTREngineController.prototype.onTouchMove = function (e) {
+    };
+    MTREngineController.prototype.onTouchEnd = function (e) {
+    };
+    return MTREngineController;
+})();
---
+++ ts/MTREngineController.ts
@@ -0,0 +1,15 @@
+/// <reference path="references.ts" />
+class MTREngineController {
+	private _playfield: PlayField;
+	private _inventory: Inventory;
+	private _objects: DrawableObject[];
+
+	constructor() {}
+
+	public draw(): void {
+
+	}
+	private onTouchStart(e: TouchEvent): void {}
+	private onTouchMove(e: TouchEvent): void {}
+	private onTouchEnd(e: TouchEvent): void {}
+}
