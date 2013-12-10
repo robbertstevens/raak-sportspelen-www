483794b9f41c1e7bb9c7850e166e3706710b1a9d
Michael stevens <m.stevens1990@gmail.com>
2013-12-10 10:54:21 +0100

    assaas

--- js/load.js
+++ js/load.js
@@ -1,10 +1,107 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-	var xhReq = new XMLHttpRequest();
-	 xhReq.open("GET", "http://raak.ggwp.eu/config.php", false);
-	 xhReq.send(null);
-	 var serverResponse = eval('(' + xhReq.responseText + ')');
-	 for (var i = serverResponse.length - 1; i >= 0; i--) {
-	 	console.log(serverResponse[i]);
-	 };
-	 	//console.log(JSON.parse(serverResponse));
+	spelen = [];
+	spelen.push({
+		game: "Softbal",
+        color: "#00ff00",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Voetbal",
+        color: "#00ff00",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Handbal",
+        color: "#0000ff",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Volleybal",
+        color: "#0000ff",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Badminton",
+        color: "#0000ff",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Handbal",
+        color: "#0000ff",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Korfbal",
+        color: "#0000ff",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	spelen.push({
+		game: "Basketbal",
+        color: "#0000ff",
+        materials: [{
+            name: "doel",
+            url: "url/to/img"
+        },
+        {
+            name: "bal",
+            url: "url/to/img"
+        }]
+	});
+	RaakStorage.storeItem("coachboard",spelen);
+
+	var blaat = RaakStorage.getItem("coachboard");
+	
+	var select = document.getElementById("spel");
+	for(index in blaat) {
+    	 select.options[select.options.length] = new Option(blaat[index].game, index);
+	}
 });
