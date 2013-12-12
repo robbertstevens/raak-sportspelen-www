a2fe6c17b56094c1ec69d087262a628e0812df1a
Tim <konijntim@gmail.com>
2013-12-12 12:34:48 +0100

    minor change

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 
 function showAbout(){
-	alert("Deze applicatie is ontwikkeld door M. Stevens, R. Stevens & T. Lagerburg in opdracht van RAAK, als invulling van de webtechstage van de opleiding ICT.")
+	alert("Deze applicatie is ontwikkeld door dhr. M. Stevens, dhr. R. Stevens & dhr. T. Lagerburg in opdracht van RAAK, als invulling van de webtechstage van de opleiding ICT.")
 }
