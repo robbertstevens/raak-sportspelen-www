5ccca1912c3aecb63c09ab83b51d47b08e70f9d8
Tim <konijntim@gmail.com>
2013-12-12 13:25:01 +0100

    niet werkende bende alert zooi doei tot maandag

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-
 function showAbout(){
-	alert("Deze applicatie is ontwikkeld door dhr. M. Stevens, dhr. R. Stevens & dhr. T. Lagerburg in opdracht van RAAK, als invulling van de webtechstage van de opleiding ICT.")
+	navigator.notification.alert(
+                                 'You are the winner!',  // message
+                                 alertDismissed,         // callback
+                                 'Game Over',            // title
+                                 'Done'                  // buttonName
+                                 );
+    
+    //alert("Deze applicatie is ontwikkeld door dhr. M. Stevens, dhr. R. Stevens & dhr. T. Lagerburg in opdracht van RAAK, als invulling van de webtechstage van de opleiding ICT.")
 }
+
+function alertDismissed(){};
