6987dc8d1f624badd007c79e23c0b5f3804c9a6e
Michael stevens <m.stevens1990@gmail.com>
2014-01-20 10:59:46 +0100

    help venster added

--- js/index.js
+++ js/index.js
@@ -12,4 +12,16 @@ function showAbout(){
     //alert("Deze applicatie is ontwikkeld door dhr. M. Stevens, dhr. R. Stevens & dhr. T. Lagerburg in opdracht van RAAK, als invulling van de webtechstage van de opleiding ICT.")
 }
 
+//functie om een native prompt scherm te weergeven.
+function showHelp(){
+	navigator.notification.alert(
+                                 'Een vierkant kan vastgezet en ontgrendeld worden door er op te dubbel tappen. de materialenlijst kan verborgen en teruggehaald worden door er op te dubbel tappen. Een item kan verwijdert worden door deze te selecteren en vervolgens op de kruis knop te drukken.',  // message
+                                 alertDismissed,         // callback
+                                 'Help tips en tricks',            // title
+                                 'Oké, ik snap het!'                  // buttonName
+                                 );
+    
+    //alert("Deze applicatie is ontwikkeld door dhr. M. Stevens, dhr. R. Stevens & dhr. T. Lagerburg in opdracht van RAAK, als invulling van de webtechstage van de opleiding ICT.")
+}
+
 function alertDismissed(){};
